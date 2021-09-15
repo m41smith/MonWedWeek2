9c6e6d1df1af4d19695970e22685b261c5f63cf0
Marcus <m41smith@student.bridgew.edu>
2021-09-15 15:32:22 -0400

    -Added Week3_RandomDemo.java to project.

---
+++ src/Week3_RandomDemo.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Week3_RandomDemo {
+    public static void main(String[] args) {
+        var randomNumberPicker = new Random();
+        var randomNumber = randomNumberPicker.nextInt(10) + 1;
+        var inputReader = new Scanner(System.in);
+        System.out.print("pick a random number from 1 to 10:");
+        var usersNumber = inputReader.nextInt();
+        while (usersNumber != randomNumber) {
+
+            if (randomNumber == usersNumber) {
+                return;
+            }
+            else if (randomNumber > usersNumber){
+                System.out.println("Unfortunately, " + usersNumber + " was too low. Guess again!");
+                usersNumber = inputReader.nextInt();
+            }
+            else {
+                System.out.println("Unfortunately, " + usersNumber + " was too high. Guess again!");
+                usersNumber = inputReader.nextInt();
+            }
+
+        }
+        System.out.println("You were right! The number was " + randomNumber);
+    }
+}
